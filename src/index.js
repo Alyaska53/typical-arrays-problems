6021caa6a1bd96627c071d127452df4ea2c1e77f
Dzmitry Buraukin <dmitry.buravkin@gmail.com>
2021-08-18 21:22:13 +0300

    fixed: bugs with no params or array is empty

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 exports.min = function min (array) {
+  if (!array || array === []) return 0;
+  
   return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
+  if (!array || array === []) return 0;
+  
   return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+  if (!array || array === []) return 0;
+  
   let sum = 0;
   array.forEach(el => sum += el);
+  
   return sum / array.length;
 }
